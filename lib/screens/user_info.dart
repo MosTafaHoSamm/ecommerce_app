476dcd2221cc28c717702aba54c4eb21d16d0070
MosTafaHosamm <mostafa.hosam1996@gmail.com>
2022-03-09 13:16:28 +0200

    create screens

---
+++ lib/screens/user_info.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class UserInfoScreen extends StatelessWidget {
+  const UserInfoScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("UserInfo Screen"),
+      ),
+    );
+  }
+}
