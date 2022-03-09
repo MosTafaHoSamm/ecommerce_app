5d080f940d857d6c86c586bef0f889b87ff93cab
MosTafaHosamm <mostafa.hosam1996@gmail.com>
2022-03-09 08:37:08 +0200

    Add ,

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -24,7 +24,7 @@ class LoginScreen extends StatelessWidget {
               border: OutlineInputBorder(),
           )
             ),
-          OutlineButton(onPressed: (){})
+          OutlineButton(onPressed: (){}),
 
         ],
       ),
